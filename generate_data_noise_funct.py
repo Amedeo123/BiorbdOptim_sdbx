2cb36369dd5511b1becdc3653817ab270a97cd81
ceglia <amedeo.ceglia@umontreal.ca>
2020-10-27 17:26:54 +0100

    Modified function to generate  EMG noise with fourier transformation and displace markers

--- generate_data_noise_funct.py
+++ generate_data_noise_funct.py
@@ -1,53 +1,91 @@
 import numpy as np
-import pickle
-import biorbd
-import scipy.io as sio
 import matplotlib.pyplot as plt
-from casadi import MX, SX, Function
+import scipy.fftpack
+import biorbd
+import pickle
+from casadi import MX, Function
+
+T = 0.5
+N = 150
+# Ps = T/N
+# Fs = 1/Ps
+# biorbd_model = biorbd.Model("arm_wt_rot_scap.bioMod")
+# with open(
+#         f"solutions/sim_ac_{int(T * 1000)}ms_{N}sn_REACH2_co_level_0.bob", 'rb'
+# ) as file:
+#     data = pickle.load(file)
+# states = data['data'][0]
+# controls = data['data'][1]
+# q_sol = states['q']
+# dq_sol = states['q_dot']
+# a_sol = states['muscles']
+# u_sol = controls['muscles']
+# u_co = u_sol
+# xf = np.linspace(-Fs/2, Fs/2, N)
+t = np.linspace(0, T, N)
+
+np.random.seed(50)
+def markers_fun(biorbd_model):
+    qMX = MX.sym('qMX', biorbd_model.nbQ())
+    return Function('markers', [qMX], [biorbd_model.markers(qMX)])
 
 
-def generate_noise(model, markers, excitations):
-    T = 0.5
-    u_co = excitations
-    t = np.linspace(0, T, u_co.shape[1])
+def generate_noise(model, q, excitations, marker_noise_level, EMG_noise_level):
     biorbd_model = model
-    # EMG : standard deviation of emg are 15%
-    EMG_w_noise = np.ndarray((biorbd_model.nbMuscles(), u_co.shape[1]))
+    q_sol = q
+    u_co = excitations
+    EMG_fft = scipy.fftpack.fft(u_co)
+    EMG_no_noise = scipy.fftpack.ifft(EMG_fft)
+    EMG_fft_noise = EMG_fft
+    for k in range(biorbd_model.nbMuscles()):
+        EMG_fft_noise[k, 0] += np.abs(np.random.normal(0, (np.real(EMG_fft_noise[k, 0]*0.08))))
+        for i in range(1, 3):
+            rand_noise = np.abs(np.random.rand()*(np.real(EMG_fft[k, i]))*EMG_noise_level/i + 0.1)
+            EMG_fft_noise[k, i] += rand_noise
+            EMG_fft_noise[k, -i] += rand_noise
+    EMG_noise = np.real(scipy.fftpack.ifft(EMG_fft_noise))
+
     for i in range(biorbd_model.nbMuscles()):
-        EMG_noise = np.random.normal(0, u_co[i, :] * 0.15, u_co[i, :].shape)
-        EMG_w_noise[i, :] = u_co[i, :] + EMG_noise
+        for j in range(EMG_noise.shape[1]):
+            if EMG_noise[i, j] < 0:
+                EMG_noise[i, j] = 0
 
     # plt.figure("Muscles controls")
     # for i in range(biorbd_model.nbMuscles()):
     #     plt.subplot(4, 5, i + 1)
+    #     plt.step(t, np.real(EMG_no_noise[i, :]))
+    #     plt.step(t, np.real(EMG_noise[i, :]))
     #     plt.step(t, u_co[i, :])
-    #     plt.step(t, EMG_w_noise[i, :], c='purple')
     #     plt.title(biorbd_model.muscleNames()[i].to_string())
-    # plt.legend(labels=['without_noise', 'with_noise'], bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+    # plt.legend(labels=['without_noise', 'with_noise', 'ref'], bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
     # plt.show()
 
-    # Marker : deviation is 0.3cm thorax, 0.4cm clav, 0.5 scap, 0.8cm rad/ulna, 1cm humerus
+    # Ref
     n_mark = biorbd_model.nbMarkers()
-    markers_w_noise = np.ndarray((3, n_mark, u_co.shape[1]))
+    get_markers = markers_fun(biorbd_model)
+    markers = np.zeros((3, biorbd_model.nbMarkers(), q_sol.shape[1]))
+    for i in range(q_sol.shape[1]):
+        markers[:, :, i] = get_markers(q_sol[:, i])
 
-    # humerus
-    for i in [0, 1, 2, 3]:
-        marker_noise = np.random.normal(0, 1e-2, markers_w_noise.shape[2])
-        markers_w_noise[:, i, :] = markers[:, i, :] + np.tile(marker_noise, 3).reshape(3, len(marker_noise))
+    for i in range(n_mark):
+        noise_position = MX(np.random.rand(3) * marker_noise_level) + biorbd_model.marker(i).to_mx()
+        biorbd_model.marker(i).setPosition(biorbd.Vector3d(noise_position[0], noise_position[1], noise_position[2]))
 
-    for i in [4, 5, 6, 7]:
-        marker_noise = np.random.normal(0, 8e-3, markers_w_noise.shape[2])
-        markers_w_noise[:, i, :] = markers[:, i, :] + np.tile(marker_noise, 3).reshape(3, len(marker_noise))
+    get_markers = markers_fun(biorbd_model)
+    markers_target_noise = np.zeros((3, biorbd_model.nbMarkers(), q_sol.shape[1]))
+    for i in range(q_sol.shape[1]):
+        markers_target_noise[:, :, i] = get_markers(q_sol[:, i])
 
+    # plot
     # plt.figure("Markers")
     # for i in range(markers.shape[1]):
-    #     plt.plot(np.linspace(0, 1, markers_w_noise.shape[2]), markers[:, i, :].T, "r--")
-    #     plt.plot(np.linspace(0, 1, markers_w_noise.shape[2]), markers_w_noise[:, i, :].T, "k")
+    #     plt.plot(np.linspace(0, 1, markers_target_noise.shape[2]), markers_target_noise[:, i, :].T, "k")
+    #     plt.plot(np.linspace(0, 1, markers.shape[2]), markers[:, i, :].T, "r--")
     # plt.xlabel("Time")
     # plt.ylabel("Markers Position")
     # plt.show()
 
-    return markers_w_noise, EMG_w_noise
+    return markers_target_noise, EMG_noise
 
 
 
