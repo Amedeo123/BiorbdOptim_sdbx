9891496fa9dc673ba1a7dbd5a8de98a540b73849
ceglia <amedeo.ceglia@umontreal.ca>
2020-10-30 10:46:51 +0100

    Added normal dist for marker noise and modified noise config for EMG

--- generate_data_noise_funct.py
+++ generate_data_noise_funct.py
@@ -38,9 +38,9 @@ def generate_noise(model, q, excitations, marker_noise_level, EMG_noise_level):
     EMG_no_noise = scipy.fftpack.ifft(EMG_fft)
     EMG_fft_noise = EMG_fft
     for k in range(biorbd_model.nbMuscles()):
-        EMG_fft_noise[k, 0] += np.abs(np.random.normal(0, (np.real(EMG_fft_noise[k, 0]*0.08))))
+        EMG_fft_noise[k, 0] += np.random.normal(0, (np.real(EMG_fft_noise[k, 0]*0.2)))
         for i in range(1, 3):
-            rand_noise = np.abs(np.random.rand()*(np.real(EMG_fft[k, i]))*EMG_noise_level/i + 0.1)
+            rand_noise = np.random.rand() * (np.real(EMG_fft[k, i]) * EMG_noise_level) + 0.1
             EMG_fft_noise[k, i] += rand_noise
             EMG_fft_noise[k, -i] += rand_noise
     EMG_noise = np.real(scipy.fftpack.ifft(EMG_fft_noise))
@@ -62,13 +62,13 @@ def generate_noise(model, q, excitations, marker_noise_level, EMG_noise_level):
 
     # Ref
     n_mark = biorbd_model.nbMarkers()
-    get_markers = markers_fun(biorbd_model)
-    markers = np.zeros((3, biorbd_model.nbMarkers(), q_sol.shape[1]))
-    for i in range(q_sol.shape[1]):
-        markers[:, :, i] = get_markers(q_sol[:, i])
+    # get_markers = markers_fun(biorbd_model)
+    # markers = np.zeros((3, biorbd_model.nbMarkers(), q_sol.shape[1]))
+    # for i in range(q_sol.shape[1]):
+    #     markers[:, :, i] = get_markers(q_sol[:, i])
 
     for i in range(n_mark):
-        noise_position = MX(np.random.rand(3) * marker_noise_level) + biorbd_model.marker(i).to_mx()
+        noise_position = MX(np.random.normal(0, marker_noise_level, 3)) + biorbd_model.marker(i).to_mx()
         biorbd_model.marker(i).setPosition(biorbd.Vector3d(noise_position[0], noise_position[1], noise_position[2]))
 
     get_markers = markers_fun(biorbd_model)
@@ -76,11 +76,11 @@ def generate_noise(model, q, excitations, marker_noise_level, EMG_noise_level):
     for i in range(q_sol.shape[1]):
         markers_target_noise[:, :, i] = get_markers(q_sol[:, i])
 
-    # plot
+
     # plt.figure("Markers")
-    # for i in range(markers.shape[1]):
+    # for i in range(markers_target_noise.shape[1]):
     #     plt.plot(np.linspace(0, 1, markers_target_noise.shape[2]), markers_target_noise[:, i, :].T, "k")
-    #     plt.plot(np.linspace(0, 1, markers.shape[2]), markers[:, i, :].T, "r--")
+    #     # plt.plot(np.linspace(0, 1, markers.shape[2]), markers[:, i, :].T, "r--")
     # plt.xlabel("Time")
     # plt.ylabel("Markers Position")
     # plt.show()
