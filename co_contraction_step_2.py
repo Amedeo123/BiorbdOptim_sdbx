4789e52edbeae58a69a05c0eb54afed777f238f1
ceglia <amedeo.ceglia@umontreal.ca>
2020-11-03 10:44:12 +0100

    Added open file and q tracking

--- co_contraction_step_2.py
+++ co_contraction_step_2.py
@@ -109,11 +109,18 @@ def prepare_ocp(
 
 T = 0.8
 Ns = 100
-
+save_data = False
 biorbd_model = biorbd.Model("arm_wt_rot_scap.bioMod")
 x0 = np.array([0., -0.2, 0, 0, 0, 0, 0, 0])
 xT = np.array([-0.2, -1.3, -0.5, 0.5, 0, 0, 0, 0])
 # muscle_idx = [9, 10, 17, 18]
+with open(
+        f"solutions/sim_ac_{int(T * 1000)}ms_{Ns}sn_REACH2_co_level_0.bob", 'rb'
+) as file:
+    data = pickle.load(file)
+states = data['data'][0]
+controls = data['data'][1]
+q_ref = states['q']
 # muscle_idx_tot = [i for i in range(biorbd_model.nbMuscles())]
 ocp = prepare_ocp(biorbd_model=biorbd_model, final_time=T, number_shooting_points=Ns, x0=x0, xT=xT, use_SX=True)
 for i in range(1, 4):
@@ -155,9 +162,10 @@ for i in range(1, 4):
         muscles_idx=np.array([9, 10, 17, 18]),
     )
     objective_functions.add(
-        Objective.Mayer.TRACK_STATE,
+        Objective.Lagrange.TRACK_STATE,
         weight=10000,
-        target=np.tile(xT[:biorbd_model.nbQ()], (u_co.shape[1] + 1, 1)).T,
+        target=q_ref,
+        # target=np.tile(xT[:biorbd_model.nbQ()], (u_co.shape[1] + 1, 1)).T,
         states_idx=np.array(range(biorbd_model.nbQ()))
     )
     ocp.update_objectives(objective_functions)
@@ -173,24 +181,25 @@ for i in range(1, 4):
             "nlp_solver_type": "SQP",
             "sim_method_num_steps": 1,
         })
-    # states, controls = Data.get_data(ocp, sol)
-    # u_final = controls['muscles']
-    # q_co = states['q']
-    # t = np.linspace(0, T, Ns + 1)
-    # plt.figure("Muscles controls")
-    # for i in range(biorbd_model.nbMuscles()):
-    #     plt.subplot(4, 5, i + 1)
-    #     plt.step(t, u_final[i, :])
-    #     plt.step(t, u_co[i, :], c='red')
-    #
-    # plt.figure("Q")
-    # for i in range(biorbd_model.nbQ()):
-    #     plt.subplot(2, 3, i + 1)
-    #     plt.plot(t, q_co[i, :])
-    #     plt.plot(t, q_sol[i, :], c='red')
-    #         # plt.title(q_name[i])
-    # # plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+    states, controls = Data.get_data(ocp, sol)
+    u_final = controls['muscles']
+    q_co = states['q']
+    t = np.linspace(0, T, Ns + 1)
+    plt.figure("Muscles controls")
+    for i in range(biorbd_model.nbMuscles()):
+        plt.subplot(4, 5, i + 1)
+        plt.step(t, u_final[i, :])
+        plt.step(t, u_co[i, :], c='red')
+
+    plt.figure("Q")
+    for i in range(biorbd_model.nbQ()):
+        plt.subplot(2, 3, i + 1)
+        plt.plot(q_co[i, :])
+        plt.plot(q_ref[i, :], 'r')
+            # plt.title(q_name[i])
+    # plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
     # plt.show()
-    ocp.save_get_data(
-        sol, f"solutions/sim_ac_{int(T * 1000)}ms_{Ns}sn_REACH2_co_level_{i}.bob"
-    )
+    if save_data:
+        ocp.save_get_data(
+            sol, f"solutions/sim_ac_{int(T * 1000)}ms_{Ns}sn_REACH2_co_level_{i}.bob"
+        )
